f064ae46def0288a3684cbc167c751db727b18ab
GustavoSLeite <gustavo.leite@sptech.school>
2025-09-11 19:15:59 -0300

    Site institucional e Telas de Autenticação (Cadastro/Login)

---
+++ frontend/clinica podologia/index.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".mvv-item ", {
+  duration: 1000,
+  distance: "400px",
+  delay: 400,
+  origin: "left",
+});
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".card", {
+  duration: 1000,
+  distance: "400px",
+  delay: 400,
+  origin: "right",
+});
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".relato", {
+  duration: 1000,
+  distance: "400px",
+  delay: 400,
+  origin: "right",
+});
